d188c537001605a285566f9d4172ca9e4caca950
Calli Herrmann <calliherrmann@gmail.com>
2023-05-17 17:12:17 -0400

    Remove text in search bar after a user searches

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,12 +1,14 @@
 //NOTE: Your DOM manipulation will occur in this file
 import { searchRecipes, retrieveInput } from "./scripts";
 
+let searchInput = document.querySelector('#search-input');
 let searchHeader = document.querySelector('#recipe-results-header')
 let recipeBoxes = document.querySelector('#recipe-results')
 
 const showSearchResults = (userValue, names, images) => {
   searchHeader.innerHTML = '';
   recipeBoxes.innerHTML = '';
+  searchInput.value = '';
   searchHeader.innerHTML += `<h1>Showing search results for "${userValue}"...</h1>`
   names.forEach((name, i) => {
     recipeBoxes.innerHTML += `
