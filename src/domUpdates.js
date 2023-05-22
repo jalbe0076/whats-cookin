2e393f722e0668463bda3ebb8fed0cbea8bfa3c6
Alec Livaditis <aleclivaditis@gmail.com>
2023-05-22 10:15:24 -0400

    fix ingredients display bug

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -89,7 +89,7 @@ const renderRecipeInfo = (recipe, data) => {
     return ingredient.quantity.unit
   })
   const ingredientDisplays = ingredients.map((ingredient, i) => {
-    return `${amounts[i]} ${units[i]} ${ingredient.name}`
+    return `${amounts[i].toFixed(2)} ${units[i]} ${ingredient.name}`
   })
   recipeIngredientList.innerText = `Ingredients: \n ${ingredientDisplays.join('\n')}`
   instructions.innerText = `Instructions: \n ${getRecipeInstructions(recipe).join('\n')}`
